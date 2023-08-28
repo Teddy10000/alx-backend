d9d7bbe93bea8f12c85f25f313a44c08ab27bd4e
Teddy10000 <Tedakrong12@gmail.com>
2023-08-28 16:11:00 +0000

    done

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -10,30 +10,54 @@ parameters.
 Page numbers are 1-indexed, i.e. the first page is page 1.
 """
 
-class CSVData:
-    def __init__(self, csv_file):
-        self.csv_file = csv_file
-        self.data = self.load_data()
 
-    def load_data(self):
-        data = []
-        with open(self.csv_file, 'r') as file:
-            reader = csv.reader(file)
-            for row in reader:
-                data.append(row)
-        return data
+from typing import Tuple, List
+import csv
+import math
 
-    def index_range(self, page, page_size):
-        start_index = (page - 1) * page_size
-        end_index = start_index + page_size
-        return start_index, end_index
 
-    def get_page(self, page=1, page_size=10):
-        assert isinstance(page, int) and page > 0, "Page should be a positive integer"
-        assert isinstance(page_size, int) and page_size > 0, "Page size should be a positive integer"
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    start index and an end index corresponding to the range of
+    """
+    # if page is 1, start at 0 and end at page_size
+    # if page is 2, start at ((page-1) * page_size) and
+    # end at (page_size * page)
+    # if page is 3, start at ((page-1) * page_size) and
+    # end at (page_size * page)
+    return ((page-1) * page_size, page_size * page)
 
-        start_index, end_index = self.index_range(page, page_size)
-        if start_index >= len(self.data):
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """return the appropriate page of the dataset"""
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+
+        # get the data from the csv
+        data = self.dataset()
+
+        try:
+            # get the index to start and end at
+            start, end = index_range(page, page_size)
+            return data[start:end]
+        except IndexError:
             return []
-
-        return self.data[start_index:end_index]
