b6584254cef98bbccdf30cebd8ceda80587841b2
Teddy10000 <Tedakrong12@gmail.com>
2023-08-28 16:16:50 +0000

    done

--- 0x00-pagination/0-simpler_helper_function.py
+++ 0x00-pagination/0-simpler_helper_function.py
@@ -14,10 +14,14 @@ Page numbers are 1-indexed, i.e. the first page is page 1.
 from typing import Tuple
 
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     start index and an end index corresponding to the range of
     """
-    # start page 
-    start = (page-1)*page_size
-    return (start, page_size * page)
+    # if page is 1, start at 0 and end at page_size
+    # if page is 2, start at ((page-1) * page_size) and
+    # end at (page_size * page)
+    # if page is 3, start at ((page-1) * page_size) and
+    # end at (page_size * page)
+    return ((page-1) * page_size, page_size * page)
